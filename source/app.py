a10575d023673c0cb6dcc83284cad6f286130489
Bharath Achar <bharath_achar@outlook.com>
2025-05-16 08:45:21 +0530

    Adding a line to app

--- source/app.py
+++ source/app.py
@@ -19,7 +19,8 @@ def details():
     return jsonify({
         'time': datetime.datetime.now(),
         'hostname': socket.gethostname(),
-        'message': 'This is my first project in Devops, Hurray! :-)'
+        'message': 'This is my first project in Devops, Hurray! :-)',
+        'message': 'just adding a line'
     })
 
 @app.route('/api/v1/healthz')
