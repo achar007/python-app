d8d528a2b186cd04ec8b43ae79d76b0b4945e569
Bharath Achar <bharath_achar@outlook.com>
2025-05-16 11:53:07 +0530

    fixing command

--- source/app.py
+++ source/app.py
@@ -20,7 +20,7 @@ def details():
         'time': datetime.datetime.now(),
         'hostname': socket.gethostname(),
         'message': 'This is my first project in Devops, Hurray! :-)',
-        'message': 'just adding a line 1234567'
+        'message': 'just adding a line 12345678'
     })
 
 @app.route('/api/v1/healthz')
